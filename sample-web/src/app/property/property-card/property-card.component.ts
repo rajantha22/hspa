46777ea8def1ff7447872f0043ba0a87bd4dfc8e
rajantha22 <hncrasteam@gmail.com>
2020-09-01 13:30:57 +0530

    HSPM

---
+++ sample-web/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+  property:any = {
+    "Id": 1,
+    "Name":"rajantha`s house",
+    "Type":"House",
+    "Price": 1200
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
